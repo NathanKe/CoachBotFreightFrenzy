beb7d58726a50066be56761853df70153c3e3f2c
NathanKe <nkearney89@gmail.com>
2021-10-11 11:20:08 -0400

    branching to RevBot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CoachBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CoachBot.java
@@ -7,7 +7,7 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class CoachBot {
     private CarouselSpinner carouselSpinner;
-    private GyroMecanumPlatform driveTrain;
+    private SixWheelPlatform driveTrain;
     private FreightArm freightArm;
     private Intake intake;
 
@@ -20,16 +20,16 @@ public class CoachBot {
 
     public void initialize(Telemetry in_telemetry, HardwareMap hardwareMap){
         carouselSpinner = new CarouselSpinner(in_telemetry, hardwareMap);
-        driveTrain = new GyroMecanumPlatform(in_telemetry, hardwareMap);
+        driveTrain = new SixWheelPlatform(in_telemetry, hardwareMap);
         freightArm = new FreightArm(in_telemetry, hardwareMap);
         intake = new Intake(in_telemetry, hardwareMap);
     }
 
     public void teleOp(Gamepad gamepad1, Gamepad gamepad2){
-        driveTrain.fieldDriveTrain(gamepad1.left_stick_x,gamepad1.left_stick_y,gamepad1.right_stick_x,1.0);
+        driveTrain.baseDriveTrain(gamepad1.left_stick_y, gamepad1.right_stick_y, 1.0);
     }
 
     public void stopAll(){
-        driveTrain.mecanumPlatform.stopAll();
+        driveTrain.stopAll();
     }
 }
